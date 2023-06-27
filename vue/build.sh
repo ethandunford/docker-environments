cc8a2ffd5a625703f3256daf44cb934a6606ab45
ethandunford <ethandunford@gmail.com>
2023-06-27 21:12:34 +0100

    build script added

---
+++ vue/build.sh
@@ -0,0 +1,17 @@
+NAME=${PWD##*/}
+
+echo "==> removing old build"
+
+rm -rf dist
+
+echo "==> building vue image"
+
+DOCKER_BUILDKIT=1 docker build -t $NAME -f Dockerfile .
+
+echo "building $NAME"
+
+docker run --rm \
+    -v "$(pwd)":/home/node/app \
+    -w /home/node/app \
+    $NAME \
+    bash -c "npm install && npm run build"
