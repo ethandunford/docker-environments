bf6443c8e395d32a818b662ed7ddc6753f7c39d0
ethandunford <ethandunford@gmail.com>
2023-06-27 19:19:12 +0100

    vue wip

---
+++ vue/run.sh
@@ -0,0 +1,22 @@
+
+NAME=${PWD##*/}
+
+echo "==> stopping and cleaning any old instances"
+docker stop $NAME
+docker rm $NAME
+
+echo "==> creating vue-network"
+docker network create -d bridge vue-network || true
+
+echo "==> building vue image"
+
+DOCKER_BUILDKIT=1 docker build -t $NAME -f Dockerfile .
+
+docker run --rm \
+    -it \
+    -v "$(pwd)/":/home/node/app \
+    -w /home/node/app \
+    --name=$NAME \
+    -p 5173:5173 \
+    $NAME \
+    bash
