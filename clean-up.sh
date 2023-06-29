0e5605d19fd5d5c1b700a670a0eb9c0fc6dcf84e
ethandunford <ethandunford@gmail.com>
2023-06-29 20:32:23 +0100

    clean up script updated

--- clean-up.sh
+++ clean-up.sh
@@ -3,7 +3,11 @@ echo "==> cleaning up!"
 for i in $(ls -d */); 
     do 
         echo "==> removing ${i%%/}"
+        docker network rm "${i%%/}-network";
         docker stop ${i%%/};
         docker rm  ${i%%/};
-        docker image ${i%%/};
+        docker stop "${i%%/}_db";
+        docker rm  "${i%%/}_db";
+        docker image rm "${i%%/}-image";
+        docker image rm "${i%%/}-db-image";
     done
